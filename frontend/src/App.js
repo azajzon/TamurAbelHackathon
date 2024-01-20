68ef218a8914d0fbe6c228324cd4036e00e22bef
Abel <abzajzon@gmail.com>
2024-01-19 22:34:24 -0500

    frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';  
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="text"
+          placeholder="Enter some text"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
       </header>
     </div>
   );
